371084b86a836a75afe437f8deed8b1f0cff0f55
Amy Li <45214377+aamyli@users.noreply.github.com>
2020-06-10 13:10:08 -0400

    Update Queue.java

--- Algorithms/Queue.java
+++ Algorithms/Queue.java
@@ -11,6 +11,8 @@ public class Queue {
 	
 	private Node front;
 	private Node rear;
+	static Stacks s1 = new Stacks();
+	static Stacks s2 = new Stacks();
 	
 	class Node {
 		int info;
@@ -56,10 +58,25 @@ public class Queue {
 		}
 	}
 	
+	public static void stackEnqueue(int item) {
+		// make sure all items are in s1, so the last item can be pushed in the correct order 
+		while (!s2.isEmpty()) {
+			s1.push(s2.pop().info);
+		}
+		s1.push(item);
+	}
+	
+	public static int stackDequeue() {
+		// pushes all items to s2 so that the first item ends on top and can be pushed out (following FIFO format)
+		while (!s1.isEmpty()) {
+			s2.push(s1.pop().info);
+		}
+		return s2.pop().info;
+	}
 	
 	public static void main(String[] args) {
 		Queue c = new Queue();
-// 		uncomment these to test out the peek and dequeue methods
+// 		uncomment these to test out the peek and dequeue methods for QUEUES
 //		c.enqueue(5);
 //		c.enqueue(4);
 //		c.enqueue(3);
@@ -68,24 +85,22 @@ public class Queue {
 //		System.out.println((c.dequeue()).info);
 //		System.out.println(c.peek());
 		
+		
 		// USING TWO STACKS TO MAKE A QUEUE
 		// goal: as queue is FIFO, and stack is LIFO, two stacks will allow for it to also be FIFO, imitating a queue
-		Stacks s1 = new Stacks();
-		Stacks s2 = new Stacks();
 		
-		// pushing values to stack 1: the order becomes 3, 4, 5, with 5 being the first to come out
-		s1.push(3);
-		s1.push(4);
-		s1.push(5);
+		// 3, 4, and 5 are pushed to s1
+		stackEnqueue(3);
+		stackEnqueue(4);
+		stackEnqueue(5);
+		// in dequeue process, 5, 4, and 3 are pushed to s2 in that order. 3, the last item in s2, is pushed out
+		System.out.println(stackDequeue());
+		// to enqueue again, the remaining 5 and 4 are returned to s1 in the order of 4, 5 (to maintain what it was before)
+		// then 6 is pushed in
+		stackEnqueue(6);
+		// to dequeue, 6, 5, and 4 are pushed to s2 in that order. 4, the last item in s2, is pushed out.
+		System.out.println(stackDequeue());
 		
-		// pop s1 to push all of the values to s2, which means that the values in s2 become 5, 4, 3
-		while (!s1.isEmpty()) {
-			s2.push(s1.pop().info);
-		}
-		
-		// thus, the first value that comes out of s2 is 3.
-		// this matches what it'd be like in a queue, in a FIFO format
-		System.out.println(s2.pop().info);
 
 	}
 
